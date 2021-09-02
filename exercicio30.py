c2a0c8f7314fcf9d1b3422b68868c3bd847c4078
Gabriel de Almeida Francisco <theghoul696@gmail.com>
2021-09-02 14:33:24 -0300

    exercicio30.py
    criação do exercicio30

---
+++ exercicio30.py
@@ -0,0 +1,4 @@
+n1 = float(input("Digite aqui o valor em real: "))
+n2 = float(input("Digite aqui a cotação do dolar: "))
+dolar = (n1/n2)
+print("O valor R${} em real da ${} dolares".format(n1,dolar))
