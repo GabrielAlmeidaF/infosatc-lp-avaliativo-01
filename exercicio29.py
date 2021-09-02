58745c868b8399ce0d50821229173380ee047f2f
Gabriel de Almeida Francisco <theghoul696@gmail.com>
2021-09-02 14:30:22 -0300

    exercicio29.py
    criação do exercicio29

---
+++ exercicio29.py
@@ -0,0 +1,6 @@
+n1 = float(input("Digite aqui a primeira nota: "))
+n2 = float(input("Digite aqui a segunda nota: "))
+n3 = float(input("Digite aqui a terceira nota: "))
+n4 = float(input("Digite aqui a quarta nota: "))
+media = (n1+n2+n3+n4)/4
+print("A média dessas 4 notas é igual a: {} ".format(media))
